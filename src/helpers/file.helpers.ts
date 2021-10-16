5b1d635963c86e0ba13e17828154843fa1c8b464
Adithya Sreyaj <adi.sreyaj@gmail.com>
2021-10-16 13:47:42 +0530

    fix: config file handling logic updated

--- src/helpers/file.helpers.ts
+++ src/helpers/file.helpers.ts
@@ -1,6 +1,5 @@
-import { promises as fs } from 'fs';
-import { ensureDir, outputJson, readJson } from 'fs-extra';
-import { commands, window } from 'vscode';
+import { ensureDir, ensureFile, outputJson, readJson } from 'fs-extra';
+import { commands } from 'vscode';
 import { VSCODE_PROJECT_CONFIG } from '../data/constants';
 import { Config } from '../interfaces/config.interface';
 import { has } from '../utils/general.util';
@@ -32,34 +31,31 @@ export const createDefaultConfigFile = async (path: string) => {
 
 export const getConfigFile = async (path: string) => {
   try {
-    const fileStat = await fs.stat(`${path}/.vscode/project.json`);
-    if (fileStat.isFile()) {
-      const config = await readJson(`${path}/.vscode/project.json`);
-      const configured = config && isConfigured(config);
-      commands.executeCommand('setContext', 'sonarqube-status.isConfigured', configured);
-      return config;
-    }
-    return null;
+    await ensureFile(`${path}/.vscode/project.json`);
+    const config = await readJson(`${path}/.vscode/project.json`);
+    const configured = config && isConfigured(config);
+    commands.executeCommand('setContext', 'sonarqube-status.isConfigured', configured);
+    return { configured, config };
   } catch (error) {
     commands.executeCommand('setContext', 'sonarqube-status.isConfigured', false);
-    return new Error('Failed to fetch config file');
+    return { configured: false, config: null };
   }
 };
 
 export const checkAndCreateConfigFileIfNeeded = async (path: string) => {
   try {
     await ensureDir(`${path}/.vscode`);
-    const configFileRes = await getConfigFile(path);
+    let config = null;
     let configured = false;
-    if (configFileRes instanceof Error) {
-      window.showErrorMessage(configFileRes.message);
-    } else if (configFileRes === null) {
-      await createDefaultConfigFile(path);
+    const configResult = await getConfigFile(path);
+    if (!configResult.configured) {
+      config = await createDefaultConfigFile(path);
     } else {
-      configured = isConfigured(configFileRes);
+      config = configResult.config;
+      configured = configResult.configured;
     }
-    commands.executeCommand('setContext', 'sonarqube-status.isConfigured', configured);
-    return configured ? configFileRes : null;
+    commands.executeCommand('setContext', 'sonarqube-status.isConfigured', configResult);
+    return { configured, config };
   } catch (error) {
     throw new Error('Failed to configure');
   }
